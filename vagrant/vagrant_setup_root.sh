406ecdfc492926a1e3d4b2049bf751a7da909f27
Rodrigo Alencar Bernardo <rodrigo@kayala.com.br>
2015-08-26 09:06:49 -0300

    add --force-yes to apt-get install

--- vagrant/vagrant_setup_root.sh
+++ vagrant/vagrant_setup_root.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #cleanup unused packages
-apt-get -y remove puppet puppet-common chef
+apt-get -y --force-yes remove puppet puppet-common chef
 
 # set locale
 locale-gen pt_BR.UTF-8
 
 # python setup
-apt-get -y install python-pip
+apt-get -y --force-yes install python-pip
 pip install virtualenv virtualenvwrapper
 
 # mongodb setup
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get update
-apt-get -y install mongodb-org
+apt-get -y --force-yes install mongodb-org
 
 #redis setup
 add-apt-repository -y ppa:rwky/redis
 apt-get update
-apt-get -y install redis-server
+apt-get -y --force-yes install redis-server
